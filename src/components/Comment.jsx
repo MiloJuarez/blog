0317358a4427129d3193c320255e368c2fbd573a
Milo Juarez <ermilo98go@gmail.com>
2022-03-09 16:09:08 -0600

    Added comment styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,22 +1,33 @@
 import React from "react";
+import "@styles/Comment.scss";
 
 const Comment = ({ comment }) => {
     return (
         <div className='Comment'>
-            <button type='button'>1</button>
-            <div className='Commment__info'>
+            <div className='Comment__info'>
+                <button type='button' className='Comment__vote'>
+                    1
+                </button>
                 <p className='Comment__author'>milo juarez gordillo</p>
                 <p className='Comment__date'>12 de feb de 2021 12:23pm</p>
             </div>
-            <div className='Comment__content'>
-                Illas semine campoque declivia oppida corpora nam inter fuit
-                discordia tellus solidumque iunctarum erat: quae terrenae ubi
-                rerum recessit iudicis aestu fixo.Illas semine campoque declivia
-                oppida corpora nam inter fuit discordia tellus solidumque
-                iunctarum erat: quae terrenae ubi rerum recessit iudicis aestu
-                fixo Illas semine campoque declivia oppida corpora nam inter
-                fuit discordia tellus solidumque iunctarum erat: quae terrenae
-                ubi
+            <div className='Comment__container'>
+                <div className='Comment__block'></div>
+                <div className='Comment__content'>
+                    <div>
+                        Illas semine campoque declivia oppida corpora nam inter
+                        fuit discordia tellus solidumque iunctarum erat: quae
+                        terrenae ubi rerum recessit iudicis aestu fixo.Illas
+                        semine campoque declivia oppida corpora nam inter fuit
+                        discordia tellus solidumque iunctarum erat: quae
+                        terrenae ubi rerum recessit iudicis aestu fixo Illas
+                        semine campoque declivia oppida corpora nam inter fuit
+                        discordia tellus solidumque iunctarum erat: quae
+                        terrenae ubi
+                        <br />
+                    </div>
+                    <button className='Comment__reply'>REPLY</button>
+                </div>
             </div>
         </div>
     );
